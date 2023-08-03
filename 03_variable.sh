6fa7912f882fd6a8a9a2ff08771a5e3a7a50ac78
Shubhadarshan Panda <72727619+codebuffdev@users.noreply.github.com>
2023-08-03 13:52:36 +0000

    working witth variable

---
+++ 03_variable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Use of variable in script 
+
+# defining variable 
+a=10
+name="dev"
+age=27
+
+# using variable 
+echo "My name is $name & age is $age"
+
+
+# Storing command output to a variable 
+HOSTNAME=$(hostname)
+echo "macine name is $HOSTNAME"
+
+
+
