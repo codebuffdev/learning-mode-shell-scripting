0af152ae0d8f08c8e8bf8dd7677ea1e6bb9b5adc
Shubhadarshan Panda <72727619+codebuffdev@users.noreply.github.com>
2023-08-04 06:07:04 +0000

    operators arithmetic

--- 12_operators_arithmetic.sh
+++ 12_operators_arithmetic.sh
@@ -1,42 +1,41 @@
-#!/bin/bash 
+#!/bin/bash
 
-# arithmetic operators -> mathematical calculations 
+# arithmetic operators -> mathematical calculations
 
 # addition (+)
 x=4
 y=10
 
-result=$[x+y]
+result=$((x + y))
 echo "add: $result"
 
 # Subtraction(-)
-result=$[x-y]
+result=$((x - y))
 echo "sub: $result"
 
 # Multiplication (*)
-result=$[x*y]
+result=$((x * y))
 echo "mul: $result"
 
 # Division (/)
-result=$[y/x]
+result=$((y / x))
 echo "division: $result"
 
-result=$[x/y]
+result=$((x / y))
 echo "division: $result"
 
 # Modulo (%)
-result=$[10%3]
+result=$((10 % 3))
 echo "modulo: $result"
 
 # Exponentiation/pow (**)
-echo "pow $[2 ** 3]"
+echo "pow $((2 ** 3))"
 
-# both can be pre & post increment / decrement 
+# both can be pre & post increment / decrement
 # increment ++
 a=5
-echo "$[a++]"
+echo "$((a++))"
 
 # decrement --
 b=4
-echo "$[--b]"
-
+echo "$((--b))"
