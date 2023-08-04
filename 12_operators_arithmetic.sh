d4bd465b0825dda7a1d9dff0f9d24afaff1c7d3e
Shubhadarshan Panda <72727619+codebuffdev@users.noreply.github.com>
2023-08-04 05:41:24 +0000

    arithematic operators

---
+++ 12_operators_arithmetic.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+# arithmetic operators -> mathematical calculations 
+
+# addition (+)
+x=4
+y=10
+
+result=$[x+y]
+echo "add: $result"
+
+# Subtraction(-)
+result=$[x-y]
+echo "sub: $result"
+
+# Multiplication (*)
+result=$[x*y]
+echo "mul: $result"
+
+# Division (/)
+result=$[y/x]
+echo "division: $result"
+
+result=$[x/y]
+echo "division: $result"
+
+# Modulo (%)
+result=$[10%3]
+echo "modulo: $result"
+
+# Exponentiation/pow (**)
+echo "pow $[2 ** 3]"
+
+# both can be pre & post increment / decrement 
+# increment ++
+a=5
+echo "$[a++]"
+
+# decrement --
+b=4
+echo "$[--b]"
+
